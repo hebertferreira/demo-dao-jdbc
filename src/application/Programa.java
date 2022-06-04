a102bf83dcfadb1780ac37f49164a0e7c88f3a06
hebertferreira <phd.hebert@outlook.com>
2022-06-04 20:01:30 -0300

    Departamento e class

---
+++ src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1,"Brinquedos");
+		System.out.println(obj);
+	}
+
+}
